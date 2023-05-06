6ca9275dfd23febf53660035693574812f9ed500
Omar Sharif <mohandesomar@gmail.com>
2023-05-06 09:57:42 +0300

    fix building

--- src/app/account/page.js
+++ src/app/account/page.js
@@ -2,10 +2,7 @@
 import styles from "./page.module.css";
 import useLanguageStore from "../../stores/languageStore";
 import { useState, useEffect } from "react";
-export const metadata = {
-  title: "Alsaif Gallary Categories",
-  description: "Shop that has what you ever wanted",
-};
+
 const Page = () => {
   const { language } = useLanguageStore();
   const [languageState, setLanguageState] = useState("");
