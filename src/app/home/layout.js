5ee17a7277e40b8c6fb4dc4eb57df54cae33dde1
Omar Sharif <mohandesomar@gmail.com>
2023-05-06 22:26:28 +0300

    try again

--- src/app/home/layout.js
+++ src/app/home/layout.js
@@ -5,6 +5,45 @@ export const metadata = {
   description: "Shop that has what you ever wanted",
 };
 
+const getCategoriesData = async () => {
+  const res = await fetch("https://v2-api.alsaifgallery.com/graphql", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: `
+    query {
+    agMegaMenuTree {
+      items {
+        id
+        name
+        icon
+        url
+        label
+        label_text_color
+        label_background_color
+        page_builder {
+          json_encoded
+        }
+        children {
+          id
+          name
+          icon
+          url
+          label
+          label_text_color
+          label_background_color
+        }
+      }
+    }
+  }
+  `,
+    }),
+  });
+  return res.json();
+};
+
 const Layout = ({ children }) => {
   return (
     <div className={styles.container}>
